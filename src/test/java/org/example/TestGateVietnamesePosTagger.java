c105b66ae0e2e74a0be8ac37ffac63155ea835c1
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-04-14 00:57:22 +0700

    hard coded file directory, check TestGateVietnamesePosTagger for more details

--- src/test/java/org/example/TestGateVietnamesePosTagger.java
+++ src/test/java/org/example/TestGateVietnamesePosTagger.java
@@ -5,7 +5,14 @@ import gate.Factory;
 import gate.LanguageAnalyser;
 import gate.util.GateException;
 import gate.test.GATEPluginTests;
+import marmot.util.Sys;
 import org.junit.Test;
+import vn.pipeline.Annotation;
+import vn.pipeline.VnCoreNLP;
+import vn.pipeline.Word;
+
+import java.io.IOException;
+import java.util.Objects;
 
 
 /**
@@ -22,9 +29,25 @@ public class TestGateVietnamesePosTagger extends GATEPluginTests {
   public void testSomething() throws GateException {
     LanguageAnalyser pr = (LanguageAnalyser)Factory.createResource("org.example.GateVietnamesePosTagger");
     try {
-      Document doc = Factory.newDocument("https://drive.google.com/file/d/1O-w0USGPdIY54c9IPnx-rrHt8hpmsE5W/view?usp=sharing");
-      pr.setDocument(doc);
-      pr.execute();
+      // TODO: vocab directory
+      // TODO: check vn/corenlp/wordsegmenter/WordSegmenter.java
+
+      String str = "Tiếng Việt, cũng gọi là tiếng Việt Nam hay Việt ngữ là ngôn ngữ của người Việt " +
+              "và là ngôn ngữ chính thức tại Việt Nam. Đây là tiếng mẹ đẻ của khoảng 85% dân cư Việt Nam " +
+              "cùng với hơn 4 triệu Việt kiều.";
+      VnCoreNLP pipeline =
+              new VnCoreNLP(
+                      new String[]{"wseg", "pos"},
+                      Objects.requireNonNull(getClass().getResource("/resources/models")).getPath()
+              );
+      Annotation vnCoreAnno = new Annotation(str);
+      pipeline.annotate(vnCoreAnno);
+      for (Word word : vnCoreAnno.getWords())
+      {
+        System.out.println(word.getForm() + " " + word.getPosTag());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     } finally {
       Factory.deleteResource(pr);
     }
