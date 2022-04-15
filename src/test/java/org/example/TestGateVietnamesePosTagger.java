472c4db02e3a3e554d9a724e36d5b9d563ec860c
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-04-15 12:08:41 +0700

    fully incorporated vncorenlp for wseg and postag

--- src/test/java/org/example/TestGateVietnamesePosTagger.java
+++ src/test/java/org/example/TestGateVietnamesePosTagger.java
@@ -5,7 +5,11 @@ import gate.LanguageAnalyser;
 import gate.util.GateException;
 import gate.test.GATEPluginTests;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import vn.pipeline.Annotation;
+import vn.pipeline.VnCoreNLP;
+import vn.pipeline.Word;
+
+import java.io.IOException;
 
 
 /**
@@ -20,9 +24,27 @@ public class TestGateVietnamesePosTagger extends GATEPluginTests {
 
   @Test
   public void testSomething() throws GateException {
-    LanguageAnalyser pr = (LanguageAnalyser)Factory.createResource("org.example.GateVietnamesePosTagger");
+    LanguageAnalyser pr =
+            (LanguageAnalyser)Factory.createResource("org.example.GateVietnamesePosTagger");
     try {
       // testing code goes here
+
+      String str = "Tiếng Việt, cũng gọi là tiếng Việt Nam hay Việt ngữ "
+              + "là ngôn ngữ của người Việt và là ngôn ngữ chính thức tại Việt Nam. "
+              + "Đây là tiếng mẹ đẻ của khoảng 85% dân cư Việt Nam cùng với hơn 4 triệu Việt kiều. "
+              + "Tiếng Việt còn là ngôn ngữ thứ hai của các dân tộc thiểu số tại Việt Nam "
+              + "và là ngôn ngữ dân tộc thiểu số được công nhận tại Cộng hòa Séc.";
+
+      VnCoreNLP pipeline = new VnCoreNLP(new String[]{"wseg", "pos"});
+      Annotation vnCoreAnno = new Annotation(str);
+      pipeline.annotate(vnCoreAnno);
+
+      for (Word word : vnCoreAnno.getWords())
+      {
+        System.out.println(word.getForm() + ": " + word.getPosTag());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     } finally {
       Factory.deleteResource(pr);
     }
